9bd6011180ad046de9b8fd81ed8b5888390f816f
Ricardo Ruiz <rricardo@unicauca.edu.co>
2025-01-23 21:59:38 -0500

    add proyect de contador visitas url

--- cdk_cont_visiturl/cdk_cont_visiturl_stack.py
+++ cdk_cont_visiturl/cdk_cont_visiturl_stack.py
@@ -1,19 +1,43 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway
 )
 from constructs import Construct
+from hitcounter import HitCounter
 
 class CdkContVisiturlStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the Lambda function resource
+        my_lambda = _lambda.Function(
+            self,
+            "HelloWorldFunction",
+            runtime = _lambda.Runtime.NODEJS_20_X, # Choose any supported Node.js runtime
+            code = _lambda.Code.from_asset("lambda"), # Points to the lambda directory
+            handler = "hello.handler", # Points to the 'hello' file in the lambda directory
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkContVisiturlQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = my_lambda,
+            proxy = False,
+        )
+        
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("hello")
+        hello_resource.add_method("GET")
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
+        )
+
+        apigateway.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
